e9813e95e5030f9a85f61791f03f029376b98c93
eyad <eyiadhisham@yahoo.com>
2020-06-21 16:48:49 +0200

    proper singeltons

--- lib/models/api.dart
+++ lib/models/api.dart
@@ -5,12 +5,14 @@ import 'package:equity_new_app/models/operationReply.dart';
 ///Responsible for communication with api endpoints and transforming Network Responses to entities
 class API
 {
+   API._default();
   factory API()
   {
     return _api;
   }
+
   
   final Network _network = Network();
   final _baseUrl = '';
-  static final _api = new API();
+  static final API _api = new API._default();
 }
--- lib/models/operations.dart
+++ lib/models/operations.dart
@@ -2,10 +2,11 @@
 ///Responsible for all Domain-related actions and the only class interacting with the Routes
 class Operations 
 {
+  Operations._default();
   factory Operations()
   {
     return _oi;
   }
   
-  static final Operations _oi = Operations();
+  static final Operations _oi = Operations._default();
 }
