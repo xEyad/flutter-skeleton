454951330a218a65abff90f7c4016be06050d5b6
eyad <eyiadhisham@yahoo.com>
2020-06-21 16:49:50 +0200

    ready network

--- lib/models/network.dart
+++ lib/models/network.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:equity_new_app/models/informationViewer.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 
 ///Responsible for all interactions with internet. All functions return HTTP.Response
