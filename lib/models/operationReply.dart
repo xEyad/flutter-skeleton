ef8d4ddfc53f7f60a8bcdff0b2f4270a57c6312c
eyad <eyiadhisham@yahoo.com>
2020-07-11 18:06:31 +0200

    more power

--- lib/models/operationReply.dart
+++ lib/models/operationReply.dart
@@ -1,10 +1,11 @@
-///represents what the API replys when performing operations on server
-class OperationReply
+///represents what the API replys when performing operations on server.
+///fill the type if you intend to return the reply with data
+class OperationReply <ReturnType>
 {
   Status status;
   String message;
-  dynamic returnData;
-  OperationReply({this.status=Status.notSet,this.message="status not set",this.returnData});
+  ReturnType returnData;
+  OperationReply(this.status,{this.message="status not set",this.returnData});
 }
 enum Status
 {  
