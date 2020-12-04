5e29bf1925bb649bcb43bdf1a5fdf0505a309960
eyad <eyiadhisham@yahoo.com>
2020-12-04 18:25:03 +0200

    operations upgraded

--- lib/models/operations.dart
+++ lib/models/operations.dart
@@ -16,11 +16,21 @@ part '../utility/diskKeys.dart';
 ///Responsible for all Domain-related actions and the only class interacting with the Routes
 class Operations 
 {
-  Operations._default();
+  Operations._default()
+  {
+    _initialize();
+  }
+
+  void _initialize() async
+  {
+    
+  }
+
   factory Operations()
   {
     return _oi;
   }
+  
   
   static final Operations _oi = Operations._default();
   static final _API _api = _API();
