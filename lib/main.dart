4b2b135a55b4300868efaa693d777b58f26fda91
eyad <eyiadhisham@yahoo.com>
2020-12-04 19:28:15 +0200

    upgraded main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:global_configuration/global_configuration.dart';
 import 'root.dart';
 
-void main()
+void main() async
 {
   WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  final cfg = await GlobalConfiguration().loadFromAsset("configuration");
   runApp(Root());
 }
